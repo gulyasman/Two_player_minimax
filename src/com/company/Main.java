b2a06dd5874423b08fac4274e2f3a0a48a8d2d63
gulyasman <teborrr@gmail.com>
2020-04-24 15:41:57 +0200

    Modify main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,6 @@ import com.company.twoplayer.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] a = {
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
-        };
-
-        State state = new State(a,1);
-        System.out.println(state.heuristics());
-
-        //PlayerVsPlayer.play();
         PlayerVsMinimax.play();
     }
 }
